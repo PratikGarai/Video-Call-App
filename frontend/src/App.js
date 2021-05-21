0a86580629d561f9cb0b7cc0b538273f11aec13b
PratikGarai <pratikgarai0208@gmail.com>
2021-05-22 03:15:25 +0530

    Context made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,42 @@
-import { AppBar, Typography } from "@material-ui/core";
+import { AppBar, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
 import Notifications from "./components/Notifications";
 import Options from "./components/Options";
 import Videoplayer from "./components/Videoplayer";
 
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <AppBar position="static" color="inherit">
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography variant="h2" align="center">Video Call App</Typography>
       </AppBar>
       <Videoplayer />
