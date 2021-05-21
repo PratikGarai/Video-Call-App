fed19f5283bbbb70830207d86fd9b25a93f83c5f
PratikGarai <pratikgarai0208@gmail.com>
2021-05-22 01:32:52 +0530

    Some stuff added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import { AppBar, Typography } from "@material-ui/core";
 import React from "react";
+import Notifications from "./components/Notifications";
+import Options from "./components/Options";
+import Videoplayer from "./components/Videoplayer";
 
 const App = () => {
   return (
     <div>
-      Hello World
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Video Call App</Typography>
+      </AppBar>
+      <Videoplayer />
+      <Options>
+        <Notifications />
+      </Options>
     </div>
   );
 }
---
+++ frontend/src/components/Videoplayer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Videoplayer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Videoplayer;
